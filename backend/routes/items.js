10743ce546a136e86d945838ab4c444790ecf0e0
“Richard Middleton” <Richard@richardmiddleton.me>
2018-09-19 11:21:16 +0200

    first commit

---
+++ backend/routes/items.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+// Item Model
+const Item = require('../models/items');
+
+router.get('/', (req, res) => {
+    Item.find()
+        .sort({ date: 1 })
+        .then(items => res.json(items))
+});
+
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name,
+        date: req.body.date,
+        id: req.body.id
+    });
+    newItem.save().then(item => res.json(item));
+});
+
+router.delete('/:id', (req, res) => {
+    Item.findOneAndRemove({ id: req.params.id },  function(err) {
+        if (err)
+            res.send(err);
+        else
+            res.json({ message: 'Item Deleted!'});
+    });
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const items = require('./backend/routes/items');
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// DB Config
+const db = require('./keys').mongoURI;
+
+// Use Routes
+app.use('/api/items', items);
+
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname + '/frontend/build/index.html'));
+});
+
+// Connect to Mongo
+mongoose.connect(db || process.env.mongoURI)
+	.then(() => console.log('MongoDB Connected'))
+	.catch(err => console.log(err));
+
+// Server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
