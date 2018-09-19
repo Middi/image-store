d9f2205edb0b0b9974e2ec27c880bac96002b0d3
“Richard Middleton” <Richard@richardmiddleton.me>
2018-09-19 15:21:57 +0200

    redirect for home removed

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,17 +5,14 @@ import { About } from './views/About';
 import { ResultsView } from './views/Results';
 import { NoMatch } from './views/NoMatch';
 import { NavBar } from './components/NavBar';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 export const Routes = () => {
   return (
     <React.Fragment>
       <NavBar />
       <Switch>
-        <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
+        <Route exact path="/" component={Home} />
         <Route exact path="/About" component={About} />
         <Route exact path="/Results" component={ResultsView} />
         <Route component={NoMatch} />
