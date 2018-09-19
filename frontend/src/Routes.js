49c2362abf8a71496519a270e238857795546b01
“Richard Middleton” <Richard@richardmiddleton.me>
2018-09-19 11:54:37 +0200

    routes

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Home from './views/Home/Home';
-import About from './views/About/About';
+import { Home } from './views/Home';
+import { About } from './views/About';
+import { TopicList } from './views/TopicList';
+import { NoMatch } from './views/NoMatch';
+import { TopicDetail } from './components/TopicDetail';
+import { NavBar } from './components/NavBar';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 export const Routes = () => {
   return (
     <div>
+      <NavBar />
       <Switch>
         <Route exact path="/Home" component={Home} />
         <Route exact path="/">
@@ -13,6 +18,9 @@ export const Routes = () => {
         </Route>
         <Route exact path="/About" component={About} />
         <Route exact path="/About" component={Home} />
+        <Route exact path="/Topics" component={TopicList} />
+        <Route path="/Topics/:topicId" component={TopicDetail} />
+        <Route component={NoMatch} />
       </Switch>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Routes } from './Routes';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Routes } from './routes'; // where we are going to specify our routes
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Router>
-        <Routes />
-    </Router>,
-    document.getElementById('root'));
-    
+  <Router>
+    <Routes />
+  </Router>,
+  document.getElementById('root'),
+);
+
 registerServiceWorker();
